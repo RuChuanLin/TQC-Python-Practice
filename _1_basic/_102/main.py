59ef6be9905fc44fd14e103b940cbce3904f2303
River Lin <ruchuanlin@gmail.com>
2020-10-18 23:12:53 +0800

    done: 102

---
+++ _1_basic/_102/main.py
@@ -0,0 +1,9 @@
+a = float(input('請輸入第一個浮點數: '))
+b = float(input('請輸入第二個浮點數: '))
+c = float(input('請輸入第三個浮點數: '))
+d = float(input('請輸入第四個浮點數: '))
+
+print("|%7.2f %7.2f|" % (a, b))
+print("|%7.2f %7.2f|" % (c, d))
+print("|%-7.2f %-7.2f|" % (a, b))
+print("|%-7.2f %-7.2f|" % (c, d))
