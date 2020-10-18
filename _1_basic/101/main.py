3231e8166a9f77a9a9a78e9c8153edce332508ff
River Lin <ruchuanlin@gmail.com>
2020-10-18 23:12:43 +0800

    edit: 101: better operation

--- _1_basic/101/main.py
+++ _1_basic/101/main.py
@@ -1,7 +1,7 @@
-a = int(input(''))
-b = int(input(''))
-c = int(input(''))
-d = int(input(''))
+a = int(input('請輸入第一個整數: '))
+b = int(input('請輸入第二個整數: '))
+c = int(input('請輸入第三個整數: '))
+d = int(input('請輸入第四個整數: '))
 
 print('|%5d %5d|' % (a, b))
 print('|%5d %5d|' % (c, d))
